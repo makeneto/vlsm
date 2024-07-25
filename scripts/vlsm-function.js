0dcbb37dfa6b76255f1ff29a813042b12ba0c6bd
Makene Neto <130509472+makeneto@users.noreply.github.com>
2024-07-25 20:13:10 +0100

    Add files via upload

---
+++ scripts/vlsm-function.js
@@ -0,0 +1,8 @@
+function validateIP(ip) {
+    const ipPattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    return ipPattern.test(ip);
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
