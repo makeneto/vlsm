37a15ae09de4df896edd77e90597d7e7df54cc7c
Makene Neto <130509472+makeneto@users.noreply.github.com>
2024-07-25 20:40:52 +0100

    Update vlsm-function.js

--- scripts/vlsm-function.js
+++ scripts/vlsm-function.js
@@ -1,8 +1,22 @@
+// Validação do IP
 function validateIP(ip) {
     const ipPattern = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
     return ipPattern.test(ip);
 }
 
+// Mudar o tema
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
 }
+
+// Copyright-Year
+function year() {
+    const currentYear = new Date()
+    const year = currentYear.getFullYear()
+
+    const copyright = document.createElement('p')
+    copyright.textContent = `© ${year} makeneDev, Inc.`
+    copyright.className = 'copyright'
+    document.querySelector('#copyright').appendChild(copyright)
+}
+year() 
